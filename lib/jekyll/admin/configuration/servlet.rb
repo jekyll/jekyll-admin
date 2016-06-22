bb3c83e0c4aea5e4b28308f692e809928091d52c
Ankur Singh <ankur13019@iiitd.ac.in>
2016-06-22 19:42:52 +0530

    use word body for consistency

--- lib/jekyll/admin/configuration/servlet.rb
+++ lib/jekyll/admin/configuration/servlet.rb
@@ -4,12 +4,12 @@ module Jekyll
       class Servlet < ApiServlet
         def do_GET(request, response)
           data = Handler.get
-          hash = {status: 200, config: data}
+          hash = {status: 200, body: data}
           send_json_response(response, hash)
         end
         def do_POST(request, response)
           json_data = parse_json_data(request)
-          @data = json_data["data"]
+          @data = json_data["body"]
           Handler.post(@data)
           hash = {status: 200, config: Handler.get}
           send_json_response(response, hash)
