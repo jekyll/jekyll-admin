1f82ddb3fa1437e2d75ce5d75dc2bf05da965afa
Ankur Singh <ankur13019@iiitd.ac.in>
2016-06-22 19:39:19 +0530

    remove useless file, add json to yaml support

--- lib/jekyll/admin/configuration/handler.rb
+++ lib/jekyll/admin/configuration/handler.rb
@@ -10,6 +10,8 @@ module Jekyll
             read_file(file_name)
           end
           def post(data)
+            # To validate the yaml and convert json if supplied
+            data = Yaml.load(data).to_yaml
             write_file(file_name,data)
           end
         end
