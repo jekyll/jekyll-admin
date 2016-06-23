77d13bc329413a8ecd53de4a805a6f9b28f7dc91
Ankur Singh <ankur13019@iiitd.ac.in>
2016-06-23 14:12:09 +0530

    fix metrics/abcsize in serve.rb by breaking the function down

--- lib/jekyll/commands/serve.rb
+++ lib/jekyll/commands/serve.rb
@@ -9,6 +9,16 @@ module Jekyll
           destination = opts["destination"]
           setup(destination)
           server = WEBrick::HTTPServer.new(webrick_opts(opts)).tap { |o| o.unmount("") }
+          mount_admin_servlets(opts, server, destination)
+          # Log the routes during startup
+          log_routes server_address(server, opts)
+          # From super
+          launch_browser server, opts if opts["open_url"]
+          boot_or_detach server, opts
+        end
+
+        def mount_servlets(opts, server, destination)
+          # Mount base jekyll server
           server.mount(opts["baseurl"], Servlet, destination, file_handler_opts)
           # Mount Admin Panel
           server.mount(opts["baseurl"]+"/admin", Jekyll::Admin::AdminServlet,
@@ -17,13 +27,12 @@ module Jekyll
           Jekyll::Admin.api_servlet_bindings.each do |route, handler|
             server.mount(opts["baseurl"]+route, *handler)
           end
-          # Log the routes during startup
-          Jekyll.logger.info "Server address:", server_address(server, opts)
-          Jekyll.logger.info "Admin Panel address:", server_address(server, opts) + "admin/"
-          Jekyll.logger.info "API Endpoint:", server_address(server, opts) + "api/"
-          # From super
-          launch_browser server, opts if opts["open_url"]
-          boot_or_detach server, opts
+        end
+
+        def log_routes(address)
+          Jekyll.logger.info "Server address:", address
+          Jekyll.logger.info "Admin Panel address:", address + "admin/"
+          Jekyll.logger.info "API Endpoint:", address + "api/"
         end
       end
     end
