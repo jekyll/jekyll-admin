b9b4e7c84b6c874781f98bc531707c3c90c37798
Ben Balter <ben.balter@github.com>
2016-07-08 17:36:50 -0400

    use sinatra-namespace to DRY things up a bit

--- lib/jekyll/admin/server/page.rb
+++ lib/jekyll/admin/server/page.rb
@@ -1,41 +1,43 @@
 module Jekyll
   module Admin
     class Server < Sinatra::Base
-      get "/pages" do
-        json site.pages.map(&:to_liquid)
-      end
+      namespace "/pages" do
+        get do
+          json site.pages.map(&:to_liquid)
+        end
 
-      get "/pages/:page_id" do
-        ensure_page
-        json page.to_liquid
-      end
+        get "/:page_id" do
+          ensure_page
+          json page.to_liquid
+        end
 
-      put "/pages/:page_id" do
-        File.write page_path, page_body
-        site.process
-        redirect to("/pages/#{params["page_id"]}")
-      end
+        put "/:page_id" do
+          File.write page_path, page_body
+          site.process
+          redirect to("/pages/#{params["page_id"]}")
+        end
 
-      delete "/pages/:page_id" do
-        ensure_page
-        File.delete page_path
-        content_type :json
-        status 200
-        halt
-      end
+        delete "/:page_id" do
+          ensure_page
+          File.delete page_path
+          content_type :json
+          status 200
+          halt
+        end
 
-      private
+        private
 
-      def page_path
-        sanitized_path params["page_id"]
-      end
+        def page_path
+          sanitized_path params["page_id"]
+        end
 
-      def page
-        site.pages.find { |p| p.path == params["page_id"] }
-      end
+        def page
+          site.pages.find { |p| p.path == params["page_id"] }
+        end
 
-      def ensure_page
-        render_404 if page.nil?
+        def ensure_page
+          render_404 if page.nil?
+        end
       end
     end
   end
