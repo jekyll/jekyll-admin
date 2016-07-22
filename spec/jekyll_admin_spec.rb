dc1e6f92c8aa1e3f423c281b1f87bb5a6d70280e
Ben Balter <ben.balter@github.com>
2016-07-22 18:00:23 -0400

    add tests for JekyllAdmin class

---
+++ spec/jekyll_admin_spec.rb
@@ -0,0 +1,8 @@
+describe Jekyll::JekyllAdmin do
+  include Rack::Test::Methods
+
+  it "returns the site" do
+    expect(described_class.site.class).to eql(Jekyll::Site)
+    expect(described_class.site.source).to eql(fixture_path("site"))
+  end
+end
