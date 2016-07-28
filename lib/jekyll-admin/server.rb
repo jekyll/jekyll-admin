60184ecae85b60adeb93ac1c0825f4f37cb1e829
Ben Balter <ben.balter@github.com>
2016-07-28 15:27:49 -0400

    properly namespace write_file and delete_file helpers

--- lib/jekyll-admin/server.rb
+++ lib/jekyll-admin/server.rb
@@ -66,18 +66,18 @@ module JekyllAdmin
       body << request_payload["body"].to_s
     end
     alias page_body document_body
-  end
 
-  def write_file(path, content)
-    path = sanitized_path(path)
-    FileUtils.mkdir_p File.dirname(path)
-    file = File.write path, content
-    site.process
-    file
-  end
+    def write_file(path, content)
+      path = sanitized_path(path)
+      FileUtils.mkdir_p File.dirname(path)
+      file = File.write path, content
+      site.process
+      file
+    end
 
-  def delete_file(path)
-    File.delete sanitized_path(path)
-    site.process
+    def delete_file(path)
+      File.delete sanitized_path(path)
+      site.process
+    end
   end
 end
