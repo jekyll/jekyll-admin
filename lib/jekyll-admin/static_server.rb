0e4b21b26c8578704eae378b371ddfee29d52af0
Ben Balter <ben.balter@github.com>
2016-07-29 10:21:25 -0400

    temporarily stub dist/index for ruby tests

--- lib/jekyll-admin/static_server.rb
+++ lib/jekyll-admin/static_server.rb
@@ -2,9 +2,17 @@ module JekyllAdmin
   class StaticServer < Sinatra::Base
     set :public_dir, File.expand_path("./public/dist", File.dirname(__FILE__))
 
+    MUST_BUILD_MESSAGE = "Front end not yet built. Run `script/build` to build.".freeze
+
     # Allow `/admin` and `/admin/`, and `/admin/*` to serve `/public/dist/index.html`
     get "/*" do
       send_file index_path
+    end
+
+    # Provide a descriptive error message in dev. if frontend is not build
+    not_found do
+      status 404
+      MUST_BUILD_MESSAGE if settings.development? || settings.test?
     end
 
     private
