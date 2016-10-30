b5a1670d10f6ee97af12f02bd5cefb0897a593b0
Ashwin Maroli <ashmaroli@gmail.com>
2016-10-30 15:04:06 +0530

    restrict meta-key length

--- src/components/metadata/MetaField.js
+++ src/components/metadata/MetaField.js
@@ -53,7 +53,8 @@ export class MetaField extends Component {
             defaultValue={fieldKey}
             className="field key-field"
             type="text"
-            placeholder="Key" />
+            placeholder="Key"
+            maxLength="21" />
           <MetaButtons
             currentType={type}
             parentType="top"
