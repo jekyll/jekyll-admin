6b992af5ba2645670586bfbc93db64564a8fc061
mertkahyaoglu <mertkahyaoglu93@gmail.com>
2016-12-24 16:07:20 +0200

    add missing content required message

--- src/actions/datafiles.js
+++ src/actions/datafiles.js
@@ -1,6 +1,5 @@
 import * as ActionTypes from '../constants/actionTypes';
 import _ from 'underscore';
-import { getParserErrorMessage } from '../constants/messages';
 import { validationError } from './utils';
 import { addNotification } from './notifications';
 import { get, put, del } from '../utils/fetch';
@@ -12,6 +11,11 @@ import {
   putDataFileUrl,
   deleteDataFileUrl
 } from '../constants/api';
+import {
+  getParserErrorMessage,
+  getContentRequiredMessage,
+  getFilenameRequiredMessage
+} from '../constants/messages';
 
 export function fetchDataFiles() {
   return (dispatch) => {
@@ -46,8 +50,8 @@ export function putDataFile(filename, data) {
         'data': 'required'
       },
       {
-        'filename.required': 'The filename is required.',
-        'data.required': 'The content is required.'
+        'filename.required': getFilenameRequiredMessage(),
+        'data.required': getContentRequiredMessage()
       }
     );
     if (errors.length) {
@@ -77,4 +81,8 @@ export function onDataFileChanged() {
   return {
     type: ActionTypes.DATAFILE_CHANGED
   };
+}
+
+function validate() {
+
 }
