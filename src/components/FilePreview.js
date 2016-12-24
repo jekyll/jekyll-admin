1d56949477441b061d906f3799df2dfcd9db9a6a
mertkahyaoglu <mertkahyaoglu93@gmail.com>
2016-12-24 16:28:47 +0200

    delete unused variable

--- src/components/FilePreview.js
+++ src/components/FilePreview.js
@@ -16,7 +16,6 @@ export default class FilePreview extends Component {
     const { file } = this.props;
     const extension = file.extname.substring(1);
     const image = /png|jpg|gif|jpeg/.test(extension);
-    let prefix = '';
     return (
       <div className="file-preview">
         <a href={file.http_url} target="_blank">
