d15c63efc9b11212b3285916adebab9142f27bb8
mertkahyaoglu <mertkahyaoglu93@gmail.com>
2016-12-24 16:23:30 +0200

    delete unused import

--- src/containers/Header.js
+++ src/containers/Header.js
@@ -2,9 +2,6 @@ import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { Link } from 'react-router';
-import _ from 'underscore';
-
-// Actions
 import { fetchConfig } from '../actions/config';
 
 export class Header extends Component {
