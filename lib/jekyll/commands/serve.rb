28c4f9c55e5b9d6e438dfc0cb24938c30abb37d1
Ben Balter <ben.balter@github.com>
2017-03-15 11:11:48 -0400

    revert changes to lib/jekyll/commands/serve.rb

--- lib/jekyll/commands/serve.rb
+++ lib/jekyll/commands/serve.rb
@@ -6,14 +6,14 @@ module Jekyll
           server = WEBrick::HTTPServer.new(webrick_opts(opts)).tap { |o| o.unmount("") }
           server.mount(opts["baseurl"], Servlet, destination, file_handler_opts)
 
-          jekyll_admin_start_up_monkey_patch(server)
+          jekyll_admin_monkey_patch(server)
 
           Jekyll.logger.info "Server address:", server_address(server, opts)
           launch_browser server, opts if opts["open_url"]
           boot_or_detach server, opts
         end
 
-        def jekyll_admin_start_up_monkey_patch(server)
+        def jekyll_admin_monkey_patch(server)
           server.mount "/admin", Rack::Handler::WEBrick, JekyllAdmin::StaticServer
           server.mount "/_api",  Rack::Handler::WEBrick, JekyllAdmin::Server
           Jekyll.logger.info "JekyllAdmin mode:", ENV["RACK_ENV"] || "production"
