913f514d3b251592662e87bc383ed67536f6838c
Ashwin Maroli <ashmaroli@gmail.com>
2017-04-28 20:52:33 +0530

    initialize DataGUI based on Metafields

---
+++ src/containers/DataGUI.js
@@ -0,0 +1,95 @@
+import React, { Component, PropTypes } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import _ from 'underscore';
+import MetaField from '../components/metadata/MetaField';
+import {
+  storeContentFields, addField, removeField, updateFieldKey, updateFieldValue,
+  moveArrayItem, convertField
+} from '../actions/metadata';
+
+export class DataGUI extends Component {
+
+  componentDidMount() {
+    const { storeContentFields, fields } = this.props;
+    storeContentFields(fields);
+  }
+
+  shouldComponentUpdate(nextProps) {
+    return nextProps.metadata !== this.props.metadata;
+  }
+
+  render() {
+    const { metadata, addField, removeField, updateFieldKey,
+      updateFieldValue, moveArrayItem, convertField, key_prefix } = this.props;
+
+    const { path, title, raw_content, ...rest } = metadata;
+
+    const metafields = _.map(rest, (field, key) => {
+      let type = "simple";
+      if (_.isObject(field)) type = "object";
+      if (_.isArray(field)) type = "array";
+      return (
+        <MetaField
+          key={key}
+          key_prefix={key_prefix}
+          type={type}
+          parentType="top"
+          fieldKey={key}
+          fieldValue={field}
+          addField={addField}
+          removeField={removeField}
+          updateFieldKey={updateFieldKey}
+          updateFieldValue={updateFieldValue}
+          moveArrayItem={moveArrayItem}
+          convertField={convertField}
+          nameAttr={`metadata['${key}']`}
+          namePrefix={`metadata`} />
+      );
+    });
+
+    return (
+      <div className="datafields">
+        {metafields}
+        <div className="data-new">
+          <a onClick={() => addField('metadata')} className="tooltip">
+            <i className="fa fa-plus-circle" /> New data field
+            <span className="tooltip-text">
+              Metadata will be stored as the <b>YAML front matter</b> within the document.
+            </span>
+          </a>
+        </div>
+      </div>
+    );
+  }
+}
+
+DataGUI.propTypes = {
+  fields: PropTypes.object.isRequired,
+  metadata: PropTypes.object.isRequired,
+  key_prefix: PropTypes.string.isRequired,
+  storeContentFields: PropTypes.func.isRequired,
+  addField: PropTypes.func.isRequired,
+  removeField: PropTypes.func.isRequired,
+  updateFieldKey: PropTypes.func.isRequired,
+  updateFieldValue: PropTypes.func.isRequired,
+  moveArrayItem: PropTypes.func.isRequired,
+  convertField: PropTypes.func.isRequired
+};
+
+const mapStateToProps = (state) => ({
+  metadata: state.metadata.metadata,
+  key_prefix: state.metadata.key_prefix
+});
+
+const mapDispatchToProps = (dispatch) => bindActionCreators({
+  storeContentFields,
+  addField,
+  removeField,
+  updateFieldKey,
+  updateFieldValue,
+  moveArrayItem,
+  convertField
+}, dispatch);
+
+export default connect(mapStateToProps, mapDispatchToProps)(DataGUI);
