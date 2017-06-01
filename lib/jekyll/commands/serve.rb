990efdaa1567627c3233d7ba4aaa5c6099e89bf6
Ashwin Maroli <ashmaroli@gmail.com>
2017-06-01 09:14:02 +0530

    warn user that --watch has been disabled

--- lib/jekyll/commands/serve.rb
+++ lib/jekyll/commands/serve.rb
@@ -16,6 +16,8 @@ module Jekyll
         def jekyll_admin_monkey_patch(server)
           server.mount "/admin", Rack::Handler::WEBrick, JekyllAdmin::StaticServer
           server.mount "/_api",  Rack::Handler::WEBrick, JekyllAdmin::Server
+          Jekyll.logger.warn "Auto-regeneration:", "disabled by JekyllAdmin."
+          Jekyll.logger.warn "", "The site will regenerate only via the Admin interface."
           Jekyll.logger.info "JekyllAdmin mode:", ENV["RACK_ENV"] || "production"
         end
       end
