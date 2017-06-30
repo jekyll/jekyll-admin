e808e0dcb71de46107dacaef74ea1f44ada7bc4c
mertkahyaoglu <mertkahyaoglu93@gmail.com>
2017-06-30 23:06:47 +0300

    even simpler

--- src/components/metadata/MetaField.js
+++ src/components/metadata/MetaField.js
@@ -7,12 +7,9 @@ import MetaButtons from './MetaButtons';
 
 export class MetaField extends Component {
 
-  componentDidMount(){
-    const { fieldKey } = this.props;
-    const isNewField = /New field/.test(fieldKey);
-    if(isNewField){
-      this.refs.field_key.select();
-    }
+  componentDidMount() {
+    const isNewField = /New field/.test(this.props.fieldKey);
+    isNewField && this.refs.field_key.select();
   }
 
   handleConvertClick(type) {
