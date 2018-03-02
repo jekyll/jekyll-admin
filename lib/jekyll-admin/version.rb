6e721f2feddee0103fe9d82839406d5641679c44
Mert Kahyaoğlu <mertkahyaoglu93@gmail.com>
2018-03-03 02:58:50 +0300

    release version

--- lib/jekyll-admin/version.rb
+++ lib/jekyll-admin/version.rb
@@ -1,3 +1,3 @@
 module JekyllAdmin
-  VERSION = "0.7.0".freeze
+  VERSION = "0.8.0".freeze
 end
--- src/constants/index.js
+++ src/constants/index.js
@@ -1,2 +1,2 @@
-export const VERSION = 'v0.7.0';
+export const VERSION = 'v0.8.0';
 export const ADMIN_PREFIX = '/admin';
