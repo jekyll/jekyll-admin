a52797940aab9330f5f7e89fb4abb9cd7ddef2ae
Alex Hoyau <a.hoyau@silexlabs.org>
2019-08-12 20:47:37 -0300

    fix loading fonts when site is in a folder i.e. has baseurl

--- src/constants/index.js
+++ src/constants/index.js
@@ -5,7 +5,8 @@ function getPrefix() {
   // TODO:
   // * 1 line does not compile (export const ADMIN_PREFIX = ...)
   // * window is not defined during tests, see why and test with baseurl
+  // MD lexoyo: window is not defined at build time, as this file is included in webpack config
   return typeof window != 'undefined'
     ? `${window.location.pathname.split('/admin')[0]}/admin`
-    : '/admin';
+    : '.';
 }
