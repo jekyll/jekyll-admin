3e175d4e16a969e8ae98e9d34e64248273f63fef
Ashwin Maroli <ashmaroli@gmail.com>
2019-09-27 15:21:29 +0530

    Release :gem: v0.9.0

--- lib/jekyll-admin/version.rb
+++ lib/jekyll-admin/version.rb
@@ -1,3 +1,3 @@
 module JekyllAdmin
-  VERSION = "0.8.1".freeze
+  VERSION = "0.9.0".freeze
 end
--- src/constants/index.js
+++ src/constants/index.js
@@ -1,2 +1,2 @@
-export const VERSION = 'v0.8.1';
+export const VERSION = 'v0.9.0';
 export const ADMIN_PREFIX = '/admin';
