6a5344d67c4ac018d738d56038c915d0f29f3e4d
Ashwin Maroli <ashmaroli@users.noreply.github.com>
2019-10-04 17:08:30 +0530

    Modernize styling of notification messages (#506)

--- src/containers/Notifications.js
+++ src/containers/Notifications.js
@@ -19,8 +19,63 @@ export class Notifications extends Component {
     });
   }
 
+  elementStyles = {
+    Containers: {
+      DefaultStyle: {
+        padding: 15,
+        width: 360,
+        textAlign: 'center',
+      },
+    },
+    Title: {
+      DefaultStyle: {
+        fontSize: 21,
+        color: '#fff',
+      },
+    },
+    NotificationItem: {
+      DefaultStyle: {
+        margin: 0,
+        padding: '9px 15px 15px',
+        fontSize: 15,
+        borderRadius: 0,
+        transition: '0.5s ease-in-out',
+      },
+      success: {
+        color: '#fff',
+        backgroundColor: '#088217',
+      },
+      error: {
+        color: '#fff',
+        backgroundColor: '#e21414',
+      },
+      warning: {
+        color: '#fff',
+        backgroundColor: '#ebad1a',
+      },
+      info: {
+        color: '#fff',
+        backgroundColor: '#369cc7',
+      },
+    },
+    Dismiss: {
+      DefaultStyle: {
+        top: 7,
+        right: 9,
+        width: 25,
+        height: 25,
+        padding: 4,
+        backgroundColor: 'transparent',
+        border: '2px solid',
+        opacity: '0.84',
+      },
+    },
+  };
+
   render() {
-    return <NotificationSystem ref="notificationSystem" />;
+    return (
+      <NotificationSystem ref="notificationSystem" style={this.elementStyles} />
+    );
   }
 }
 
