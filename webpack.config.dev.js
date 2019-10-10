20a9ef2c91b1631749091949184e1436c0019786
Mert Kahyaoğlu <mertkahyaoglu93@gmail.com>
2019-10-10 16:49:42 +0300

    Add version as global from package.json (#510)
    * Add version from package.json as global
    * Add VERSION to jest globals

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -5,7 +5,8 @@ import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
 
 const GLOBALS = {
   'process.env.NODE_ENV': JSON.stringify('development'),
-  __DEV__: true
+  __DEV__: true,
+  VERSION: JSON.stringify(require("./package.json").version)
 };
 
 export default {
