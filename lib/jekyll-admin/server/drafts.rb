70f3122080bcd15f5e8faf7d254ec8ad02e8fb6d
Ashwin Maroli <ashmaroli@users.noreply.github.com>
2019-11-21 15:47:25 +0530

    Use Jekyll::PageWithoutAFile instead of our class (#518)
    `Jekyll::PageWithoutAFile` is available since Jekyll 3.7.0

--- lib/jekyll-admin/server/drafts.rb
+++ lib/jekyll-admin/server/drafts.rb
@@ -97,7 +97,7 @@ module JekyllAdmin
       end
 
       def html_content?
-        draft = JekyllAdmin::PageWithoutAFile.new(
+        draft = Jekyll::PageWithoutAFile.new(
           site,
           site.source,
           "_drafts",
