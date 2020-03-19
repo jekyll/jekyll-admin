0ae74301368793c00c7b4ac51d430f953eb82fe1
mertkahyaoglu <mertkahyaoglu93@gmail.com>
2020-03-19 09:03:13 +0300

    Move build folder rather than changing its path

--- config-overrides.js
+++ config-overrides.js
@@ -1,8 +1,6 @@
 const rewireWebpackBundleAnalyzer = require('react-app-rewire-webpack-bundle-analyzer');
 
 module.exports = function override(config, env) {
-  config.output.path = `${__dirname}/lib/jekyll-admin/public`;
-
   if (env === 'production') {
     config = rewireWebpackBundleAnalyzer(config, env, {
       analyzerMode: 'static',
