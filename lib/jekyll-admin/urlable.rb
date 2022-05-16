f61bc1234be9e8e64089c0303c1b7eb16020cc4b
Corey Gillen <corey.gillen@oregonstate.edu>
2022-05-16 12:17:02 -0700

    Replace jekyll_admin arrary access with dig

--- lib/jekyll-admin/urlable.rb
+++ lib/jekyll-admin/urlable.rb
@@ -61,18 +61,18 @@ module JekyllAdmin
     end
 
     def scheme
-      JekyllAdmin.site.config["jekyll_admin"]["scheme"] ||
+      JekyllAdmin.site.config.dig("jekyll_admin", "scheme") ||
         JekyllAdmin.site.config["scheme"] ||
         "http"
     end
 
     def host
-      JekyllAdmin.site.config["jekyll_admin"]["host"] ||
+      JekyllAdmin.site.config.dig("jekyll_admin", "host") ||
         JekyllAdmin.site.config["host"].sub("127.0.0.1", "localhost")
     end
 
     def port
-      JekyllAdmin.site.config["jekyll_admin"]["port"] ||
+      JekyllAdmin.site.config.dig("jekyll_admin", "port") ||
         JekyllAdmin.site.config["port"]
     end
   end
